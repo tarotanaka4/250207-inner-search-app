e30f3661a8311fcf36623f8c2815bdd725303378
yonekatsuya <seiryo.soccer.ky@i.softbank.jp>
2024-12-09 10:59:23 +0900

    fix

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-
 from dotenv import load_dotenv
 import streamlit as st
 from langchain_community.document_loaders import PyMuPDFLoader
